28660c930fc65844dbb9bba093efc512f166f9a0
rickeaton <rickeaton@sierraserviceproject.org>
2012-04-04 16:29:53 -0700

    Update to language in confirmation/invitation email.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,16 +4,26 @@ class ReportsController < ApplicationController
   load_and_authorize_resource :liaison, :parent => false
 
   def church_and_liaison(scope = nil)
-    @headers = get_headers
-    @rows = get_rows
+    @headers = get_headers_full_report
+    @rows = get_rows_full_report
 
     respond_to do |format|
       format.csv { create_csv("liaisons_and_churches-#{Time.now.strftime("%Y%m%d")}.csv") }
       format.html { @title = 'Liaisons and Churches'}
     end
+    end
+
+  def scheduled_liaisons
+    @headers = get_headers_scheduled_liaisons
+    @rows = get_rows_scheduled_liaisons
+
+    respond_to do |format|
+      format.csv { create_csv("scheduled liaisons-#{Time.now.strftime("%Y%m%d")}.csv") }
+      format.html { @title = 'Scheduled Liaison'}
+    end
   end
 
-  def get_headers
+  def get_headers_full_report
   # Find all liaisons and associated churches. Will contain duplicate church info if more than one liaison is
   # assigned to a church
 
@@ -29,7 +39,7 @@ class ReportsController < ApplicationController
       return @headers
   end
 
-  def get_rows
+  def get_rows_full_report
 
       @rows = []
       liaisons = Liaison.all
@@ -45,6 +55,28 @@ class ReportsController < ApplicationController
           end
           @rows << row
       end
+      logger.debug @rows
+      return @rows
+  end
+
+  def get_headers_scheduled_liaisons
+
+      @headers = []
+      @headers << "Liaison Name" << "Church" << "Liaison Email"
+      logger.debug @headers.inspect
+      return @headers
+  end
+
+  def get_rows_scheduled_liaisons
+
+      @rows = []
+      liaisons = ScheduledGroup.all
+      liaisons.each do |l|
+          row = []
+          row << l.liaison.name << l.church.name << l.liaison.email1
+          @rows << row
+      end
+
       logger.debug @rows
       return @rows
    end
