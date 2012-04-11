bebf35e60063fd34fe36e2fd5382eca7f3a57e93
rickeaton <rickeaton@sierraserviceproject.org>
2012-04-11 10:42:47 -0700

    Changed grade menus and corrected stylesheet problem.

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -1,6 +1,6 @@
 class Roster < ActiveRecord::Base
   attr_accessible :id, :group_id, :group_type
-    belongs_to :scheduled_group
+    belongs_to :scheduled_group, :foreign_key => :group_id
     has_many :roster_items
 
     validates :group_id, :group_type, :presence => true
