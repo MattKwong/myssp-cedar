9fad84e4a35d4a5027fd40f3ce6d362fb7b4926c
Rick Eaton <rickeaton@sierraserviceproject.org>
2012-09-17 19:02:59 -0700

    Troubleshooting asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module Spoic3
   class Application < Rails::Application
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += %w(active_admin_custom.css custom.css style.css active_admin.css)
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w(active_admin.js)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.1'
