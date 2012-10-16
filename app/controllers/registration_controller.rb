feb72b5e873d2b93870baa0c5bcbb4049ae49dc9
Rick Eaton <rickeaton@sierraserviceproject.org>
2012-10-15 18:16:33 -0700

    Changes to schedule report.

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -187,13 +187,6 @@ class RegistrationController < ApplicationController
     build_schedule(params[:reg_or_sched], params[:type])
   end
 
-  def program_session
-    @requests = Registration.find_all_by_request1(params[:id])
-    session = Session.find(params[:id])
-    @session_week = Period.find(session.period.id).name
-    @session_site = Site.find(session.site_id).name
-  end
-
 
   def get_limit_info
     #Let Summer senior high be the default in the event that an invalid value is passed
