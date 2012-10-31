6f07dce94a992f8fa0874df1c83c89ff4c65df70
Rick Eaton <rickeaton@sierraserviceproject.org>
2012-10-30 22:14:37 -0700

    Rewrote invoice and statement logic for view; moved to model out of controller. Need to refactor invoice report and pdf.

--- app/controllers/scheduled_groups_controller.rb
+++ app/controllers/scheduled_groups_controller.rb
@@ -198,20 +198,20 @@ class ScheduledGroupsController < ApplicationController
 
   def invoice
     @group= ScheduledGroup.find(params[:id])
-    liaison_name = Liaison.find(@group.liaison_id).name
-    site_name = Site.find(Session.find(@group.session_id).site_id).name
-    period_name = Period.find(Session.find(@group.session_id).period_id).name
-    start_date = Period.find(Session.find(@group.session_id).period_id).start_date
-    end_date = Period.find(Session.find(@group.session_id).period_id).end_date
-    session_type = SessionType.find(Session.find(@group.session_id).session_type_id).name
-    invoice = calculate_invoice_data(@group)
-    church = Church.find(@group.church_id)
-    invoice_items = create_invoice_items(invoice)
-
-    @screen_info = {:scheduled_group => @scheduled_group, :invoice_items => invoice_items,
-      :site_name => site_name, :period_name => period_name, :start_date => start_date,
-      :end_date => end_date,  :session_type => session_type, :invoice_data => invoice,
-      :liaison_name => liaison_name, :church_info => church}
+    #liaison_name = Liaison.find(@group.liaison_id).name
+    #site_name = Site.find(Session.find(@group.session_id).site_id).name
+    #period_name = Period.find(Session.find(@group.session_id).period_id).name
+    #start_date = Period.find(Session.find(@group.session_id).period_id).start_date
+    #end_date = Period.find(Session.find(@group.session_id).period_id).end_date
+    #session_type = SessionType.find(Session.find(@group.session_id).session_type_id).name
+    #invoice = calculate_invoice_data(@group)
+    #church = Church.find(@group.church_id)
+    #invoice_items = create_invoice_items(invoice)
+    #
+    #@screen_info = {:scheduled_group => @scheduled_group, :invoice_items => invoice_items,
+    #  :site_name => site_name, :period_name => period_name, :start_date => start_date,
+    #  :end_date => end_date,  :session_type => session_type, :invoice_data => invoice,
+    #  :liaison_name => liaison_name, :church_info => church}
     @title = "Invoice for: #{@group.name}"
   end
 
@@ -599,7 +599,9 @@ private
       redirect_to admin_registrations_path
     end
   end
+
 #TODO Refactor this out -duplicates code in liaisons controller
+
   def calculate_invoice_data(group)
     original_reg = Registration.find(group.registration_id)
     payment_schedule = PaymentSchedule.find(Session.find(group.session_id).payment_schedule_id)
