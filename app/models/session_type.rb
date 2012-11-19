1502039559443dcdfb7a883cc2001b80d43fcae2
Rick Eaton <rickeaton@sierraserviceproject.org>
2012-11-19 10:42:05 -0800

    Correction to auto schedule logic, added edits to request values and added "year" column to request display.

--- app/models/session_type.rb
+++ app/models/session_type.rb
@@ -61,19 +61,19 @@ class SessionType < ActiveRecord::Base
     if self.senior_high?
       results << {:description => 'Unscheduled', :number_groups =>  Registration.where("scheduled <> 't' AND created_at > ? AND group_type_id = 2", '2012-09-01'.to_date).count,
                 :number_requests => (Registration.current.senior_high_unscheduled.map &:requested_total).sum }
-      results << {:description => 'Scheduled', :number_groups => ScheduledGroup.high_school.active_program.count,
-                :number_requests => (ScheduledGroup.high_school.active_program.map &:current_total).sum }
+      results << {:description => 'Scheduled', :number_groups => ScheduledGroup.active_program.where('group_type_id = ?', 2).count,
+                :number_requests => (ScheduledGroup.active_program.where('group_type_id = ?', 2).map &:current_total).sum }
       for i in 1..10 do
-        results << {:description => "Scheduled in choice ##{i}", :number_groups => ScheduledGroup.high_school.active_program.find_all_by_scheduled_priority(i).count,
-                  :number_requests => (ScheduledGroup.high_school.active_program.find_all_by_scheduled_priority(i).map &:current_total).sum }
+        results << {:description => "Scheduled in choice ##{i}", :number_groups => ScheduledGroup.active_program.where('scheduled_priority = ? and group_type_id = ?', i, 2).count,
+                  :number_requests => (ScheduledGroup.high_school.active_program.where('scheduled_priority = ? and group_type_id = ?', i, 2).map &:current_total).sum }
       end
         over_count = 0
         Session.senior_high.active.each { |g| if g.scheduled_total > target then  over_count += 1 end }
         results << {:description => 'Number session over target', :number_groups => over_count,
                     :number_requests => '' }
     else
-      results << {:description => 'Unscheduled', :number_groups => Registration.current.junior_high_unscheduled.count,
-                :number_requests => (Registration.current_unscheduled.map &:requested_total).sum }
+      results << {:description => 'Unscheduled', :number_groups => Registration.where("scheduled <> 't' AND created_at > ? AND group_type_id = 3", '2012-09-01'.to_date).count,
+                :number_requests => (Registration.where("scheduled <> 't' AND created_at > ? AND group_type_id = 3", '2012-09-01'.to_date).map &:requested_total).sum }
       results << {:description => 'Scheduled', :number_groups => ScheduledGroup.junior_high.active_program.count,
                 :number_requests => (ScheduledGroup.junior_high.active_program.map &:current_total).sum }
       for i in 1..10 do
