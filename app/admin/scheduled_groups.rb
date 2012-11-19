35452d8ed6160a2a5237b159732e910256948239
Rick Eaton <rickeaton@sierraserviceproject.org>
2012-11-19 15:05:14 -0800

    Added priority choice to schedule group list.

--- app/admin/scheduled_groups.rb
+++ app/admin/scheduled_groups.rb
@@ -22,10 +22,9 @@ ActiveAdmin.register ScheduledGroup do
           link_to scheduled_group.session.period, admin_session_path(period) end }
        row("Start Date") { scheduled_group.session.period.start_date }
        row("End Date") { scheduled_group.session.period.end_date }
+       row("Priority") { scheduled_group.scheduled_choice}
        row("2nd Payment Count") { scheduled_group.second_payment_total }
        row("2nd Payment Date") { scheduled_group.second_payment_date }
-       #do |d|
-       #   d.second_payment_date.strftime("%m/%d/%y") end }
      end
    end
  end
