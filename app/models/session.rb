8ecd6b240194f449d0ac918e1bef0ec07945c6d6
Rick Eaton <rickeaton@sierraserviceproject.org>
2012-11-29 15:03:57 -0800

    Styling of availability table.

--- app/models/session.rb
+++ app/models/session.rb
@@ -468,7 +468,9 @@ class Session < ActiveRecord::Base
     #Matrix type is Registration, Scheduled or Availability
     #Assumes summer domestic. Sites are the rows; weeks are the columns
       @site_names = Site.order(:listing_priority).find_all_by_active_and_summer_domestic(true, true).map { |s| s.name}
-      @period_names = Period.order(:start_date).find_all_by_active_and_summer_domestic(true, true).map { |p| p.name}
+      period_names = Period.order(:start_date).find_all_by_active_and_summer_domestic(true, true).map { |p| p.name}
+      period_sh_dates = Period.order(:start_date).find_all_by_active_and_summer_domestic(true, true).map { |p| "#{p.start_date.strftime("%b %d")} - #{p.end_date.strftime("%b %d")}"}
+      period_jh_dates = Period.order(:start_date).find_all_by_active_and_summer_domestic(true, true).map { |p| "#{p.start_date.strftime("%b %d")} - #{(p.end_date - 1).strftime("%b %d")}"}
       @title = @page_title = "#{program_type} Schedule: #{matrix_type}"
 
     #Assign a ordinal value to each row and column
@@ -478,16 +480,16 @@ class Session < ActiveRecord::Base
       end
 
       @period_ordinal = Array.new
-      for i in 0..@period_names.size - 1 do
-        @period_ordinal[i] = @period_names[i]
+      for i in 0..period_names.size - 1 do
+        @period_ordinal[i] = period_names[i]
       end
 
     #Create and populate matrices.
-      @registration_matrix = Array.new(@site_names.size + 1){ Array.new(@period_names.size + 1, 0)}
-      @scheduled_matrix = Array.new(@site_names.size + 1){ Array.new(@period_names.size + 1, 0)}
-      @session_id_matrix = Array.new(@site_names.size + 1){ Array.new(@period_names.size + 1, 0)}
-      @avail_matrix = Array.new(@site_names.size + 1){ Array.new(@period_names.size + 1, 0)}
-      @senior_high = Array.new(@site_names.size + 1){ Array.new(@period_names.size + 1, true)}
+      @registration_matrix = Array.new(@site_names.size + 1){ Array.new(period_names.size + 1, 0)}
+      @scheduled_matrix = Array.new(@site_names.size + 1){ Array.new(period_names.size + 1, 0)}
+      @session_id_matrix = Array.new(@site_names.size + 1){ Array.new(period_names.size + 1, 0)}
+      @avail_matrix = Array.new(@site_names.size + 1){ Array.new(period_names.size + 1, 0)}
+      @senior_high = Array.new(@site_names.size + 1){ Array.new(period_names.size + 1, true)}
 
       Registration.all(:conditions => "(request1 IS NOT NULL) AND (scheduled = 'f')").each do |r|
         @session = Session.find(r.request1)
@@ -515,7 +517,7 @@ class Session < ActiveRecord::Base
 
     #Populate the availability_matrix by traversing the scheduled_matrix
       for i in 0..@site_names.size - 1 do
-        for j in 0..@period_names.size - 1 do
+        for j in 0..period_names.size - 1 do
           if @session_id_matrix[i][j] > 0
             session = Session.find(@session_id_matrix[i][j])
             @avail_matrix[i][j] = session.available
@@ -533,16 +535,16 @@ class Session < ActiveRecord::Base
       @reg_total = 0
       @sched_total = 0
       for i in 0..@site_names.size - 1 do
-        for j in 0..@period_names.size - 1 do
+        for j in 0..period_names.size - 1 do
           @reg_total += @registration_matrix[i][j]
           @sched_total += @scheduled_matrix[i][j]
         end
-        @registration_matrix[i][@period_names.size] = @reg_total
-        @scheduled_matrix[i][@period_names.size] = @sched_total
+        @registration_matrix[i][period_names.size] = @reg_total
+        @scheduled_matrix[i][period_names.size] = @sched_total
         @reg_total = @sched_total = 0
       end
 
-      for j in 0 ..@period_names.size do
+      for j in 0 ..period_names.size do
         for i in 0..@site_names.size - 1 do
           @reg_total = @reg_total + @registration_matrix[i][j]
           @sched_total = @sched_total + @scheduled_matrix[i][j]
@@ -555,20 +557,20 @@ class Session < ActiveRecord::Base
     #Grand totalS
       @reg_total = @sched_total = 0
       for i in 0..@site_names.size - 1 do
-        @reg_total = @reg_total + @registration_matrix[i][@period_names.size]
-        @sched_total = @sched_total + @scheduled_matrix[i][@period_names.size]
+        @reg_total = @reg_total + @registration_matrix[i][period_names.size]
+        @sched_total = @sched_total + @scheduled_matrix[i][period_names.size]
       end
-      @registration_matrix[@site_names.size][@period_names.size] = @reg_total
-      @scheduled_matrix[@site_names.size][@period_names.size] = @sched_total
+      @registration_matrix[@site_names.size][period_names.size] = @reg_total
+      @scheduled_matrix[@site_names.size][period_names.size] = @sched_total
 
-      @period_names << "Total"
+      period_names << "Total"
       @site_names << "Total"
 
     #Replace zeros in cells which do not represent an active session
       for i in 0..@site_names.size - 2 do
         site = Site.active.summer_domestic.find_by_name(@site_names[i]).id
-        for j in 0..@period_names.size - 2 do
-          period = Period.active.summer_domestic.find_by_name(@period_names[j]).id
+        for j in 0..period_names.size - 2 do
+          period = Period.active.summer_domestic.find_by_name(period_names[j]).id
           if Session.where('site_id = ? AND period_id = ?', site, period).size == 0
             @registration_matrix[i][j] = "-"
             @scheduled_matrix[i][j] = "-"
@@ -578,10 +580,11 @@ class Session < ActiveRecord::Base
       end
 
     #Return matrices
-      @matrices = { :site_count => @site_names.size - 1, :period_count => @period_names.size - 1,
-                    :site_names => @site_names, :period_names => @period_names,
+      @matrices = { :site_count => @site_names.size - 1, :period_count => period_names.size - 1,
+                    :site_names => @site_names, :period_names => period_names,
                     :registration_matrix => @registration_matrix, :scheduled_matrix => @scheduled_matrix,
                     :session_id_matrix => @session_id_matrix, :matrix_type => matrix_type, :program_type => program_type,
-                    :avail_matrix => @avail_matrix, :senior_high => @senior_high}
+                    :avail_matrix => @avail_matrix, :senior_high => @senior_high, :period_sh_dates => period_sh_dates,
+                    :period_jh_dates => period_jh_dates}
   end
 end
